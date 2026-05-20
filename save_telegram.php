24cf3091612a0b99a5c345987bf0cfa91e387780
azraining <azraining04@gmail.com>
2026-05-21 00:05:45 +0800

    Update save_telegram.php

--- save_telegram.php
+++ save_telegram.php
@@ -86,6 +86,8 @@ if (strpos($text, "/start") === 0) {
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
         $response = curl_exec($ch);
 
